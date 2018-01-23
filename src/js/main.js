bba465a3e83ce9d3af073852e9b1995ff84ebf73
nilesh singh <nileshing26@gmail.com>
2018-01-23 22:03:05 +0000

    Changes to the styling page

--- src/js/main.js
+++ src/js/main.js
@@ -2,201 +2,60 @@ require('../css/style.scss');
 import anime from 'animejs';
 import scrollmonitor from 'scrollmonitor';
 
+var morphing= ()=>{
+	// anime({
+	//  targets: '#morphing .polymorph',
+	//  points: [
+	// 	 { value: '70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369' },
+	// 	 { value: '70 6 119.574 60.369 100.145 117.631 39.855 117.631 55.426 68.369' },
+	// 	 { value: '70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369' },
+	// 	 { value: '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369' }
+	//  ],
+	//  easing: 'easeOutQuad',
+	//  duration: 2000,
+	//  loop: true
+ // });
 
+ // anime({
+ //  targets: '#morphing-test .pathmorph',
+ //  d: [
+ // 	 // { value: 'M943,535.703899 C1030.25859,535.703899 1101,464.5838 1101,376.847389 C1101,289.444376 1030.59507,219.703899 943,219.703899 C855.404926,219.703899 785,289.444376 785,376.847389 C785,464.5838 855.741407,535.703899 943,535.703899 Z' },
+	//  { value: 'M1101,220.828427 L787.828427,534 L1101,534 L1101,220.828427 Z'},
+	//  { value: 'M785,220.828427 L785,534 L1098.17157,534 L785,220.828427 Z'},
+	//  { value: 'M943,220.472136 L786.236068,534 L1099.76393,534 L943,220.472136 Z'},
+	//  { value: 'M814,280 L814,534 L1072,534 L1072,280 L814,280 Z'}
+ //
+ //  ],
+ //  easing: 'easeOutQuad',
+ //  duration: 2000,
+ //  loop: true
+ // });
 
-const DOM ={};
-DOM.svg = document.querySelector('.morph');
-DOM.shapeEl = DOM.svg.querySelector('polygon');
-DOM.contentElems = Array.from(document.querySelectorAll('.content-wrap'));
-DOM.footer= document.querySelector('.content--related');
-console.log(DOM.contentElems);
-const contentElemsTotal = DOM.contentElems.length;
-const shapes = [
-	{
-		points: '700,84.4 1047.1,685.6 352.9,685.6 352.9,685.6 352.9,685.6 352.9,685.6',
-		scaleX: .8,
-		scaleY: .9,
-		rotate: 0,
-		tx: 0,
-		ty: 0,
-		fill: {
-			color: 'none',
-			duration: 500,
-			easing: 'linear'
-		},
-		animation: {
-			points: {
-				duration: 500,
-				easing: 'easeOutExpo'
-			},
-			svg: {
-				duration: 1500,
-				easing: 'easeOutElastic'
-			}
-		}
-	},
-	{
-		points: '983.4,101.6 983.4,668.4 416.6,668.4 416.6,101.9 416.6,101.9 416.6,101.9',
-		scaleX: .7,
-		scaleY: .7,
-		rotate: 90,
-		tx: -100,
-		ty: 100,
-		fill: {
-			color: 'none',
-			duration: 500,
-			easing: 'linear'
-		},
-		animation: {
-			points: {
-				duration: 500,
-				easing: 'easeOutExpo'
-			},
-			svg: {
-				duration: 1500,
-				easing: 'easeOutElastic'
-			}
-		}
-	},
-	{
-		points: '890.9,54.3 1081.8,385 890.9,715.7 509.1,715.7 318.2,385 509.1,54.3',
-		scaleX: 1,
-		scaleY: 1,
-		rotate: -45,
-		tx: 0,
-		ty: -50,
-		fill: {
-			color: 'none',
-			duration: 500,
-			easing: 'linear'
-		},
-		animation: {
-			points: {
-				duration: 500,
-				easing: 'easeOutExpo'
-			},
-			svg: {
-				duration: 1500,
-				easing: 'easeOutElastic'
-			}
-		}
-	},
-	{
-		points: '983.4,101.6 779,385 983.4,668.4 416.6,668.4 611,388 416.6,101.9',
-		scaleX: 1,
-		scaleY: 1,
-		rotate: 145,
-		tx: 100,
-		ty: -50,
-		fill: {
-			color: 'none',
-			duration: 500,
-			easing: 'linear'
-		},
-		animation: {
-			points: {
-				duration: 500,
-				easing: 'easeOutExpo'
-			},
-			svg: {
-				duration: 1500,
-				easing: 'easeOutElastic'
-			}
-		}
-	},
-	{
-		points: '983.4,101.6 1255,385 983.4,668.4 416.6,668.4 157,368 416.6,101.9',
-		scaleX: .7,
-		scaleY: .7,
-		rotate: -70,
-		tx: -50,
-		ty: 50,
-		fill: {
-			color: 'none',
-			duration: 500,
-			easing: 'linear'
-		},
-		animation: {
-			points: {
-				duration: 500,
-				easing: 'easeOutExpo'
-			},
-			svg: {
-				duration: 1500,
-				easing: 'easeOutElastic'
-			}
-		}
-	},
-	{
-		points: '983.4,101.6 983.4,668.4 416.6,668.4 416.6,101.9 416.6,101.9 416.6,101.9',
-		scaleX: 1.2,
-		scaleY: 1.2,
-		rotate: 20,
-		tx: 0,
-		ty: 0,
-		fill: {
-			color: 'none',
-			duration: 500,
-			easing: 'linear'
-		},
-		animation: {
-			points: {
-				duration: 500,
-				easing: 'easeOutExpo'
-			},
-			svg: {
-				duration: 1500,
-				easing: 'easeOutElastic'
-			}
-		}
-	}
-];
-
-const createScrollWatchers = () =>{
-	DOM.contentElems.forEach((el,pos)=>{
-		// console.log(pos);
-		const scrollElemToWatch = DOM.contentElems[pos];
-		pos = pos ? pos : contentElemsTotal;
-		console.log(scrollElemToWatch);
-		const watcher = scrollmonitor.create(scrollElemToWatch);
-
-		watcher.enterViewport(()=>{
-				anime({
-					targets: DOM.shapeEl,
-					duration: shapes[pos].animation.points.duration,
-					easing: shapes[pos].animation.points.easing,
-					elasticity: shapes[pos].animation.points.elasticity || 0,
-					points: shapes[pos].points,
-					fill: {
-						value: shapes[pos].fill.color,
-						duration: shapes[pos].fill.duration,
-						easing: shapes[pos].fill.easing
-					}
-				});
-		})
-	})
+ anime({
+   targets: '#morphing .pathmorph',
+   points: [
+     { d: 'M791,231.828427 L791,545 L1104.17157,545 L791,231.828427 Z' },
+     { d: 'M791,230 L791,546 L1107,546 L1107,230 L791,230 Z' },
+     { d: '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369' }
+   ],
+   easing: 'easeOutQuad',
+   duration: 2000,
+   loop: true
+ });
 }
 
-const initShapeEl = () =>{
-	anime.remove(DOM.svg);
-	anime({
-			targets: DOM.svg,
-			duration: 1,
-			easing: 'linear',
-			scaleX: shapes[0].scaleX,
-			scaleY: shapes[0].scaleY,
-			translateX: shapes[0].tx+'px',
-			translateY: shapes[0].ty+'px',
-			rotate: shapes[0].rotate+'deg'
-		});
-}
+
+
+
+
+
 
 
 
 var init = () =>{
+
+	morphing();
 	// initShapeEl();
-	createScrollWatchers();
-	console.log('hello');
 }
 
 init();
